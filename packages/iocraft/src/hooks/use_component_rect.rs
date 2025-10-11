3d141f683bf793080e4aec160251389036c2715d
barsoosayque <shtoshich@gmail.com>
2025-10-12 01:44:11 +0700

    improve documentation and use ref for rect

--- packages/iocraft/src/hooks/use_component_rect.rs
+++ packages/iocraft/src/hooks/use_component_rect.rs
@@ -1,41 +1,47 @@
 use taffy::Rect;
 
-use crate::{ComponentDrawer, Hook, Hooks};
+use crate::{
+    hooks::{Ref, UseRef},
+    ComponentDrawer, Hook, Hooks,
+};
 
 mod private {
     pub trait Sealed {}
     impl Sealed for crate::Hooks<'_, '_> {}
 }
 
-/// `UseComponentRect` is a hook that returns the current component canvas position and size.
+/// `UseComponentRect` is a hook that returns the current component's canvas position and size
+///  from the previous frame.
 ///
 /// See [`ComponentDrawer::canvas_position`] and [`ComponentDrawer::size`] for more info.
+///
+/// <div class="warning">For the first time rendering, it will return exactly (0, 0, 0, 0) rectangle !</div>
 pub trait UseComponentRect<'a>: private::Sealed {
     /// Returns the curent component canvas position and size in form of a [`Rect`].
-    fn use_component_rect(&mut self) -> Rect<u16>;
+    fn use_component_rect(&mut self) -> Ref<Rect<u16>>;
 }
 
 impl<'a> UseComponentRect<'a> for Hooks<'a, '_> {
-    fn use_component_rect(&mut self) -> Rect<u16> {
-        self.use_hook(UseComponentRectImpl::default).rect
+    fn use_component_rect(&mut self) -> Ref<Rect<u16>> {
+        let rect = self.use_ref_default();
+        self.use_hook(|| UseComponentRectImpl { rect }).rect
     }
 }
 
-#[derive(Default)]
 struct UseComponentRectImpl {
-    rect: Rect<u16>,
+    rect: Ref<Rect<u16>>,
 }
 
 impl Hook for UseComponentRectImpl {
     fn pre_component_draw(&mut self, drawer: &mut ComponentDrawer) {
         let size = drawer.size();
         let position = drawer.canvas_position();
-        self.rect = Rect {
+        self.rect.set(Rect {
             left: position.x as u16,
             right: position.x as u16 + size.width,
             top: position.y as u16,
             bottom: position.y as u16 + size.height,
-        };
+        });
     }
 }
 
@@ -48,18 +54,15 @@ mod tests {
 
     #[component]
     fn MyComponent(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
-        let mut should_exit = hooks.use_state(|| false);
         let mut system = hooks.use_context_mut::<SystemContext>();
-        let rect = hooks.use_component_rect();
+        let mut frame = hooks.use_state(|| 0);
+        let rect = hooks.use_component_rect().get();
 
-        hooks.use_terminal_events(move |event| match event {
-            TerminalEvent::Resize(..) => should_exit.set(true),
-            _ => {}
-        });
-
-        if should_exit.get() {
+        // Notice that we have to wait one frame for the correct size and position.
+        if frame.get() >= 1 {
             system.exit();
         }
+        frame += 1;
 
         element! {
             Text(content: format!("{}:{}:{}:{}", rect.left, rect.right, rect.top, rect.bottom))
@@ -77,9 +80,7 @@ mod tests {
                 height: 50,
             ) { MyComponent }
         )
-        .mock_terminal_render_loop(MockTerminalConfig::with_events(futures::stream::iter(
-            vec![TerminalEvent::Resize(40, 50)],
-        )))
+        .mock_terminal_render_loop(MockTerminalConfig::default())
         .map(|c| c.to_string())
         .collect::<Vec<_>>()
         .await;
